5ddecdaa1b1e37a7bdeb2a6697e376b1d74cde95
BJakeLando <bjlando@gmail.com>
2022-08-28 21:05:47 -0700

    Final Report
    This is a to do list as a final report

---
+++ practice.js
@@ -0,0 +1,21 @@
+const studentInput = document.getElementById("studentName");
+
+function addStudent(){
+
+    let student = document.getElementById("studentName").value;
+    console.log("Adding..." + student);
+    displayStudent(student);
+    clearInput();
+}
+
+function displayStudent(newStudent){
+    console.log("Adding..." + newStudent);
+    document.getElementById("student-list").innerHTML += `<li class="item">${newStudent}</li>`;
+
+}
+
+function clearInput(){
+    studentInput.value ="";
+    studentInput.focus();
+
+}
